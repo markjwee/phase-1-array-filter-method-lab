102c930dac5a973530da84adaa6b6844869a02bf
Mark <mark.j.wee@gmail.com>
2022-01-03 17:57:37 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+
+function findMatching(array){
+    let newArray = array.filter(drivers => drivers == array)
+    return newArray
+}
+
+// function findMatchingFilter(array){
+//     let newArray = filter(array, function(drivers) {
+//         return array == drivers
+//     });
+//     return newArray
+// }
+
+// function findMatching(arrayOfDrivers){
+//     function conditional(array){
+        
+//     }
+//     let result = array.map(conditional)
+//         return result
+// }
+
+function fuzzyMatch(arrayOfObjects){
+    let newArray = []
+}
+
+function matchName(arrayOfObjects){
+    let newArray = []
+}
